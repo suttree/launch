a91483e595dff5f9349b553db87ffe55488936e5
duncan <duncan@compy386.local>
2026-09-05 12:10:28 +0100

    Move Menu into its own repository

--- Menu/Package.swift
+++
@@ -1,9 +0,0 @@
-// swift-tools-version: 5.9
-import PackageDescription
-
-let package = Package(
-    name: "Menu",
-    platforms: [.macOS(.v13)],
-    products: [.executable(name: "Menu", targets: ["Menu"])],
-    targets: [.executableTarget(name: "Menu")]
-)
