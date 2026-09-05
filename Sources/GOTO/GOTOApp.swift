4c3aa6e07ae5bb46d6e7aa9f4fdd19bba38f8493
duncan <duncan@compy386.local>
2026-09-05 10:10:02 +0100

    Use Earth for Photos drop target

--- Sources/GOTO/GOTOApp.swift
+++ Sources/GOTO/GOTOApp.swift
@@ -422,8 +422,8 @@ struct BarView: View {
                         }
                     }
                 }
-                Text("🧅")
-                    .font(.system(size: 15))
+                Text("🌍")
+                    .font(.system(size: 13))
                     .frame(width: 40, height: 40)
                     .background(isPhotoDropTargeted ? selectionHighlight : .clear)
                     .onDrop(of: [UTType.image.identifier, UTType.fileURL.identifier], isTargeted: $isPhotoDropTargeted) { providers in
